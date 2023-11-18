8a4869c9d516189725ec7b6b5b1f1bff059099ea
Alexey Filimonov <filimonov.alks@gmail.com>
2023-11-18 22:46:54 +0300

    Add Journal class

---
+++ src/call_center/journal.cc
@@ -0,0 +1,100 @@
+#include "journal.h"
+
+#include <fstream>
+#include <format>
+
+#include <boost/uuid/uuid_io.hpp>
+
+#include "core/utils/date_time.h"
+
+namespace call_center {
+
+namespace expr = boost::log::expressions;
+namespace date_time_utils = core::utils::date_time;
+
+Journal::Journal(const Configuration &configuration)
+    : file_name_(ReadFileName()),
+      sink_(MakeSink()),
+      logger_({}, sink_),
+      configuration_(configuration) {
+}
+
+void Journal::Formatter(const boost::log::record_view &rec, boost::log::formatting_ostream &out) {
+  out << rec[expr::message];
+}
+
+std::string Journal::ReadFileName() const {
+  return configuration_.GetProperty<std::string>(kFileNameKey).value_or(kDefaultFileName);
+}
+
+std::string Journal::FormatCallDetailedRecord(const CallDetailedRecord &cdr) {
+  return std::vformat("{};{};{};{};{};{};{};{}",
+                      std::make_format_args(
+                          FormatTimePoint(cdr.GetReceiptTime()),
+                          FormatUuid(cdr.GetId()),
+                          cdr.GetCallerPhoneNumber(),
+                          FormatTimePoint(cdr.GetEndProcessingTime()),
+                          to_string(cdr.GetStatus()),
+                          FormatTimePoint(cdr.GetStartProcessingTime()),
+                          FormatUuid(cdr.GetOperatorId()),
+                          FormatDuration(cdr.GetProcessingDuration()
+                          )));
+}
+
+void Journal::AddRecord(const CallDetailedRecord &cdr) {
+  UpdateSink();
+  auto result = FormatCallDetailedRecord(cdr);
+  logger_.Info() << result;
+}
+
+std::string Journal::FormatTimePoint(const std::optional<CallDetailedRecord::TimePoint> &time_point) {
+  if (time_point)
+    return FormatTimePoint(time_point.value());
+  else
+    return "";
+}
+
+std::string Journal::FormatTimePoint(const CallDetailedRecord::TimePoint &time_point) {
+  return std::vformat("{0:%F} {0:%T}",
+                      std::make_format_args(
+                          time_point));
+}
+
+std::string Journal::FormatDuration(const CallDetailedRecord::Duration &duration) {
+  return std::vformat("{0:%T}",
+                      std::make_format_args(
+                          duration));
+}
+
+std::string Journal::FormatDuration(const std::optional<CallDetailedRecord::Duration> &duration) {
+  if (duration)
+    return FormatDuration(*duration);
+  else
+    return "";
+}
+
+std::string Journal::FormatUuid(const boost::uuids::uuid &uuid) {
+  return boost::uuids::to_string(uuid);
+}
+
+std::string Journal::FormatUuid(const std::optional<boost::uuids::uuid> &uuid) {
+  if (uuid)
+    return boost::uuids::to_string(*uuid);
+  else
+    return "";
+}
+
+void Journal::UpdateSink() {
+  auto new_file_name = ReadFileName();
+  if (new_file_name != file_name_) {
+    file_name_ = std::move(new_file_name);
+    sink_ = MakeSink();
+  }
+}
+
+log::Sink Journal::MakeSink() {
+  return {boost::make_shared<std::ofstream>(file_name_, std::ios_base::app),
+          log::SeverityLevel::kTrace, SIZE_MAX, Journal::Formatter};
+}
+
+} // call_center
---
+++ src/call_center/journal.h
@@ -0,0 +1,53 @@
+#ifndef CALL_CENTER_SRC_CALL_CENTER_JOURNAL_H_
+#define CALL_CENTER_SRC_CALL_CENTER_JOURNAL_H_
+
+#include <boost/log/sinks.hpp>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/time_facet.hpp>
+
+#include <string>
+#include <optional>
+#include <ostream>
+
+#include "log/severity_level.h"
+#include "log/sink.h"
+#include "log/logger.h"
+#include "call_detailed_record.h"
+#include "configuration.h"
+
+namespace call_center {
+
+class Journal {
+ public:
+  Journal(const Configuration &configuration);
+
+  void AddRecord(const CallDetailedRecord &cdr);
+
+ private:
+  static constexpr const auto kFileNameKey = "journal_file_name";
+  static constexpr const auto kDefaultFileName = "journal.csv";
+
+  std::string file_name_;
+  log::Sink sink_;
+  log::Logger logger_;
+  const Configuration &configuration_;
+
+  static void Formatter(const boost::log::record_view &rec, boost::log::formatting_ostream &out);
+  static std::string FormatCallDetailedRecord(const CallDetailedRecord &cdr);
+  static std::string FormatTimePoint(const CallDetailedRecord::TimePoint &time_point);
+  static std::string FormatTimePoint(const std::optional<CallDetailedRecord::TimePoint> &time_point);
+  static std::string FormatUuid(const boost::uuids::uuid &uuid);
+  static std::string FormatUuid(const std::optional<boost::uuids::uuid> &uuid);
+  static std::string FormatDuration(const CallDetailedRecord::Duration &duration);
+  static std::string FormatDuration(const std::optional<CallDetailedRecord::Duration> &duration);
+
+  std::string ReadFileName() const;
+  void UpdateSink();
+  log::Sink MakeSink();
+};
+
+}
+
+#endif //CALL_CENTER_SRC_CALL_CENTER_JOURNAL_H_
