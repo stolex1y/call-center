a85560ae48a26ccd73c2de75949aecc564d99747
Alexey Filimonov <filimonov.alks@gmail.com>
2023-11-18 23:07:02 +0300

    Update cmake lists

--- cmake/SetupEnv.cmake
+++ cmake/SetupEnv.cmake
@@ -3,4 +3,7 @@ include_guard(GLOBAL)
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
+add_compile_options(-fsanitize=address)
+add_link_options(-fsanitize=address)
+
 message(STATUS "C++ compiler: ${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}(${CMAKE_CXX_COMPILER})")
