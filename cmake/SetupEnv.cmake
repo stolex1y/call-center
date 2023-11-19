cf0dbface4fc0edce09bdd49f6eaea9090ce437e
Alexey Filimonov <filimonov.alks@gmail.com>
2023-11-19 14:30:40 +0300

    Fix test taget

--- cmake/SetupEnv.cmake
+++ cmake/SetupEnv.cmake
@@ -3,6 +3,8 @@ include_guard(GLOBAL)
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
+enable_testing()
+
 add_compile_options(-fsanitize=address)
 add_link_options(-fsanitize=address)
 
