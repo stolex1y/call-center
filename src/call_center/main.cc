8f2d902f59120eb19c941f749c5e0f63eee5592d
Alexey Filimonov <filimonov.alks@gmail.com>
2023-12-07 13:00:23 +0300

    Add MainSink to main and port updating

--- src/call_center/main.cc
+++ src/call_center/main.cc
@@ -1,13 +1,15 @@
 #include "call_center.h"
-#include "configuration.h"
-#include "configuration_updater.h"
+#include "configuration/configuration.h"
+#include "configuration/configuration_updater.h"
 #include "core/http/http_server.h"
 #include "core/tasks/task_manager_impl.h"
 #include "journal.h"
+#include "main_sink.h"
 #include "repository/call/call_repository.h"
 #include "repository/metrics/metrics_repository.h"
 
 using namespace call_center;
+using namespace call_center::config;
 using namespace call_center::core::http;
 using namespace call_center::log;
 using namespace call_center::core;
@@ -15,15 +17,22 @@ using namespace call_center::core::tasks;
 using namespace call_center::core::qs::metrics;
 using namespace call_center::repository;
 
-int main() {
-  const LoggerProvider logger_provider(std::make_shared<Sink>(SeverityLevel::kTrace));
+static constexpr auto kPortKey = "http_server_port";
+static constexpr uint16_t kPortDefault = 8080;
 
-  constexpr auto port = static_cast<unsigned short>(8080);
+int main() {
+  const auto main_sink = MainSink::Create();
+  const LoggerProvider logger_provider(main_sink);
+  const auto configuration = Configuration::Create(logger_provider);
+
+  const auto port = configuration->GetNumber<uint16_t>(kPortKey, kPortDefault, 0, UINT16_MAX);
   const auto address = net::ip::address_v4::any();
 
-  const auto configuration = Configuration::Create(logger_provider);
-  auto task_manager = TaskManagerImpl::Create(configuration, logger_provider);
-  ConfigurationUpdater::Create(configuration, task_manager, logger_provider)->StartUpdating();
+  const auto task_manager = TaskManagerImpl::Create(configuration, logger_provider);
+  const auto configuration_updater =
+      ConfigurationUpdater::Create(configuration, task_manager, logger_provider);
+  configuration_updater->StartUpdating();
+  main_sink->StartSinkUpdate(configuration_updater);
   const auto operator_provider = [&task_manager, &configuration, &logger_provider] {
     return Operator::Create(task_manager, configuration, logger_provider);
   };
